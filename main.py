69f5d20300f4fbdde4db00a874f7d1c150c806ff
Lane Wofford <134661022+Lanerbop@users.noreply.github.com>
2025-04-04 11:20:36 -0500

    Create map function that returns a dictionary for correct words

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ chosen_word = input("Which word have you chosen? ")
 correct_letters = input("Which letters were fully correct? Ex: 'aet' if 'a', 'e', and 't' were fully correct. ")
 wrong_placement_letters = input("Which letters were valid but in the incorrect position? Ex: 'bp' if 'b' and 'p' were in the incorrect position ")
 
-
-
-
-
+def map_correct_letters(word, correct_letters):
+    """
+    Return a dictionary containing correct letters in their correct
+    position. Format: {index: letter}
+    """
+    correct_letters_map = {}
+    for count, char in enumerate(word):
+        if char in correct_letters:
+            correct_letters_map[count] = char
+    return correct_letters_map
 
