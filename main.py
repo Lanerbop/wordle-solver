9fde2835c22d8e341ce342aa8cf93d49dd3da7dd
Lane Wofford <134661022+Lanerbop@users.noreply.github.com>
2025-04-04 12:03:49 -0500

    Fix bug that would cause incorrect letter mapping for repeated characters

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ with open("valid-words.txt") as f:
     valid_words = [line.rstrip('\n') for line in f]
 
 chosen_word = input("Which word have you chosen? ")
-correct_letters = input("Which letters were fully correct? Ex: 'aet' if 'a', 'e', and 't' were fully correct. ")
+# TODO: Add input validation, such as len + characters only
+correct_letters = input("Which letters were fully correct? Replace an incorrect letters with an underscore. Ex: '_ra__' ")
+# TODO: Add input validation, such as len
 wrong_placement_letters = input("Which letters were valid but in the incorrect position? Ex: 'bp' if 'b' and 'p' were in the incorrect position ")
 
 def map_correct_letters(word, correct_letters):
@@ -11,8 +13,13 @@ def map_correct_letters(word, correct_letters):
     position. Format: {index: letter}
     """
     correct_letters_map = {}
-    for count, char in enumerate(word):
-        if char in correct_letters:
-            correct_letters_map[count] = char
+    for index, char in enumerate(word):
+        if char == correct_letters[index]:
+            correct_letters_map[index] = char
     return correct_letters_map
 
+# def valid_word(word, correct_letters, wrong_place_letters):
+#     """
+#     Return true if the given word is a possible answer at this stage of the game.
+#     Else, return false.
+#     """
